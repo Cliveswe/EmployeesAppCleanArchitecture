b3b93ac0eb2b3c18fb4113e2f3ad06cc7d93678a
Iryna <iryna.chorna@hotmail.com>
2025-05-27 13:41:05 +0200

    Added test for Add method

--- EmployeeApp.Application.Tests/EmployeeTests.cs
+++ EmployeeApp.Application.Tests/EmployeeTests.cs
@@ -39,7 +39,7 @@ public class EmployeeTests
                 new Employee { Id = 10, Name = "test", Email = "test@asdasd"},
                 new Employee { Id = 20, Name = "test2", Email = "test2@asdasd" }
                 ]);
-           var employeeService = new EmployeeService(employeeRepository.Object);
+        var employeeService = new EmployeeService(employeeRepository.Object);
         // Act
         var employees = employeeService.GetAll();
 
@@ -47,6 +47,29 @@ public class EmployeeTests
         Assert.NotNull(employees);
         Assert.Equal(2, employees.Length);
         employeeRepository.Verify(e => e.GetAll(), Times.Exactly(1));
+
+    }
+
+    [Fact]
+    public void Add_ShouldAddNewEmployeeToListOfEmployees()
+    {
+        // Arrange
+        var employeeRepository = new Mock<IEmployeeRepository>();
+        employeeRepository
+            .Setup(e => e.GetAll())
+            .Returns([
+                new Employee { Id = 10, Name = "test", Email = "test@asdasd" },
+                new Employee { Id = 20, Name = "test2", Email = "test2@asdasd" }
+            ]);
+        var employeeService = new EmployeeService(employeeRepository.Object);
+        // Act
+        var newEmployee = new Employee { Id = 30, Name = "New Employee", Email = "new.employee@test.fake.se" };
+        employeeService.Add(newEmployee);
+
+        var employees = employeeService.GetAll();
+
+        //Assert
+        Assert.Equal(3, employees.Length);
 
     }
 }
