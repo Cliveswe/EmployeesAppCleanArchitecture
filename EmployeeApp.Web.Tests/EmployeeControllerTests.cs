f2f753ed3725c6e24100d7458347243a5714d93b
zhelyazkovse <zhelyazkovse@gmail.com>
2025-05-27 15:53:35 +0200

    Add unit tests for EmployeesController
    - Introduced new using directive for view models.
    - Added `Create_ValidModel_ShouldAddEmployeeAndRedirect` test to verify employee creation and redirection.
    - Added `Details_Return_View_Model` test to ensure correct view model is returned for employee details.
    - Utilized Moq for mocking `IEmployeeService` in tests.
    - Maintained existing structure of the test class.

--- EmployeeApp.Web.Tests/EmployeeControllerTests.cs
+++ EmployeeApp.Web.Tests/EmployeeControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeesApp.Application.Employees.Interfaces;
 using EmployeesApp.Domain.Entities;
 using EmployeesApp.Web.Controllers;
+using EmployeesApp.Web.Views.Employees;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -29,5 +30,49 @@ namespace EmployeeApp.Web.Tests
 
 
         }
-    }
+        [Fact]
+        public void Create_ValidModel_ShouldAddEmployeeAndRedirect()
+        {
+            // Arrange
+            var mockService = new Mock<IEmployeeService>();
+
+            var controller = new EmployeesController(mockService.Object);
+
+            var viewModel = new CreateVM {
+                Name = "Test",
+                Email = "test@example.com",
+                BotCheck = 4 
+            };
+
+            // Act
+            var result = controller.Create(viewModel);
+
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+
+            mockService.Verify(s => s.Add(It.Is<Employee>(
+                e => e.Name == viewModel.Name && e.Email == viewModel.Email)), Times.Once);
+        }
+
+        [Fact]
+
+        public void Details_Return_View_Model()
+        {
+            // Arrange
+            var employeeService = new Mock<IEmployeeService>();
+            employeeService
+                .Setup(e => e.GetById(10))
+                .Returns(new Employee { Id = 10, Name = "test", Email = "test@asdasd" });
+            var controller = new EmployeesController(employeeService.Object);
+            // Act
+            var result = controller.Details(10);
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<DetailsVM>(viewResult.Model);
+            var model = (DetailsVM)viewResult.Model;
+            Assert.Equal(10, model.Id);
+        }
+    } 
 }
+
