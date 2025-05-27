4e1be1a0ddf6640a9af310aa63c7ec2ca8f66b27
zhelyazkovse <zhelyazkovse@gmail.com>
2025-05-27 12:14:37 +0200

    Refactor EmployeeTests and add GetAllEmployees test
    Modified the `GetById_ValidId_ReturnEmployee` test for improved readability by adjusting brace placement. Added a new test method `GetAllEmployees_ReturnsListOfEmployees` to verify the retrieval of all employees from the repository, including setup for mock data and necessary assertions.

--- EmployeeApp.Application.Tests/EmployeeTests.cs
+++ EmployeeApp.Application.Tests/EmployeeTests.cs
@@ -8,7 +8,8 @@ namespace EmployeeApp.Application.Tests;
 public class EmployeeTests
 {
     [Fact]
-    public void GetById_ValidId_ReturnEmployee() {
+    public void GetById_ValidId_ReturnEmployee()
+    {
 
         // Arrange
         var employeeRepository = new Mock<IEmployeeRepository>();
@@ -25,5 +26,27 @@ public class EmployeeTests
         Assert.NotNull(employee);
         Assert.Equal(10, employee.Id);
         employeeRepository.Verify(x => x.GetById(10), Times.Exactly(1));
+    }
+
+    [Fact]
+    public void GetAllEmployees_ReturnsListOfEmployees()
+    {
+        // Arrange
+        var employeeRepository = new Mock<IEmployeeRepository>();
+        employeeRepository
+            .Setup(e => e.GetAll())
+            .Returns([
+                new Employee { Id = 10, Name = "test", Email = "test@asdasd"},
+                new Employee { Id = 20, Name = "test2", Email = "test2@asdasd" }
+                ]);
+           var employeeService = new EmployeeService(employeeRepository.Object);
+        // Act
+        var employees = employeeService.GetAll();
+
+        // Assert
+        Assert.NotNull(employees);
+        Assert.Equal(2, employees.Length);
+        employeeRepository.Verify(e => e.GetAll(), Times.Exactly(1));
+
     }
 }
