50f09e4914c80a8f10f9108844f1a1620a8c54a1
Clive <Cliveswe@users.noreply.github.com>
2025-05-27 11:42:22 +0200

    Refactor EmployeeTests and add GetById test case
    Updated `EmployeeTests.cs` to include necessary using directives. Renamed the test method to `GetByid_ValidId_ReturnEmployee` and implemented a new test case to verify employee retrieval by a valid ID. The test now uses a mock repository and includes assertions to ensure the employee is returned correctly and the repository method is called as expected.

--- EmployeeApp.Application.Tests/EmployeeTests.cs
+++ EmployeeApp.Application.Tests/EmployeeTests.cs
@@ -1,9 +1,29 @@
-﻿namespace EmployeeApp.Application.Tests;
+﻿using EmployeesApp.Application.Employees.Interfaces;
+using EmployeesApp.Application.Employees.Services;
+using EmployeesApp.Domain.Entities;
+using Moq;
+
+namespace EmployeeApp.Application.Tests;
 
 public class EmployeeTests
 {
     [Fact]
-    public void Test1() {
+    public void GetByid_ValidId_ReturnEmployee() {
 
+        // Arrange
+        var employeeRepository = new Mock<IEmployeeRepository>();
+        employeeRepository
+                .Setup(x => x.GetById(10))
+                .Returns(new Employee { Id = 10, Name = "John Doe", Email = "John.Doe@lakdj" });
+
+        var employeeService = new EmployeeService(employeeRepository.Object);
+
+        // Act
+        var employee = employeeService.GetById(10);
+
+        // Assert
+        Assert.NotNull(employee);
+        Assert.Equal(10, employee.Id);
+        employeeRepository.Verify(x => x.GetById(10), Times.Exactly(1));
     }
 }
