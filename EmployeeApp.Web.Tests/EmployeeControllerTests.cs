cb21acb65c6c1b0783d951e2a18373aae2bd0cf4
zhelyazkovse <zhelyazkovse@gmail.com>
2025-05-27 15:13:22 +0200

    Add EmployeeApp.Web.Tests project and unit tests
    Updated the solution to include a new test project `EmployeeApp.Web.Tests` with necessary configurations.
    Created the project file `EmployeeApp.Web.Tests.csproj` with dependencies for unit testing, including `xunit`, `Moq`, and `coverlet.collector`.
    Added `EmployeeControllerTests` class to test the `Index` action of the `EmployeesController`, verifying it returns a `ViewResult`.

---
+++ EmployeeApp.Web.Tests/EmployeeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using EmployeesApp.Application.Employees.Interfaces;
+using EmployeesApp.Domain.Entities;
+using EmployeesApp.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EmployeeApp.Web.Tests
+{
+    public class EmployeeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_View_Model()
+        {
+            // Arrange
+            var employeeService = new Mock<IEmployeeService>();
+            employeeService
+                .Setup(e => e.GetAll())
+            .Returns([
+                new Employee { Id = 10, Name = "test", Email = "test@asdasd"},
+                new Employee { Id = 20, Name = "test2", Email = "test2@asdasd" }
+                ]);
+            var controller = new EmployeesController(employeeService.Object);
+            // Act
+            var result = controller.Index();
+
+            // Assert
+
+            Assert.IsType<ViewResult>(result);
+
+
+        }
+    }
+}
